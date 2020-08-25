4af9bc3c17d5c82efe2f3841e13bfa6e212d0c4d
guoheng01 <guoheng45@163.com>
2020-08-25 15:55:14 +0800

    springboot环境启动测试

---
+++ code/demo/itcast-springboot/src/main/java/cn/itcast/springboot/TestApplication.java
@@ -0,0 +1,14 @@
+package cn.itcast.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 引导类：springboot的入口
+ */
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class,args);
+    }
+}
