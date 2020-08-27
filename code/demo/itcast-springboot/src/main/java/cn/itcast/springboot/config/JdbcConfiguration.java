ac9cb4c01c2bbc3d9335d098e6a84e5b473c6a7a
guoheng01 <guoheng45@163.com>
2020-08-27 10:52:24 +0800

    直接在@bean方法上使用@ConfigurationProperties(prefix="jdbc")

--- code/demo/itcast-springboot/src/main/java/cn/itcast/springboot/config/JdbcConfiguration.java
+++ code/demo/itcast-springboot/src/main/java/cn/itcast/springboot/config/JdbcConfiguration.java
@@ -1,26 +1,19 @@
 package cn.itcast.springboot.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 @Configuration//声明一个类是一个java配置类，相当于一个xml文件
-@EnableConfigurationProperties(JdbcProperties.class)
 public class JdbcConfiguration {
 
-//    @Autowired
-//    private JdbcProperties jdbcProperties;
-
     @Bean //将方法的返回值注入到spring容器
-    public DataSource dataSource(JdbcProperties jdbcProperties){//bean方法的形参注入
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource dataSource(){
         DruidDataSource dataSource =   new DruidDataSource();
-        dataSource.setUrl(jdbcProperties.getUrl());
-        dataSource.setDriverClassName(jdbcProperties.getDriverClassName());
-        dataSource.setUsername(jdbcProperties.getUserName());
-        dataSource.setPassword(jdbcProperties.getPassWord());
         return dataSource;
     }
 }
